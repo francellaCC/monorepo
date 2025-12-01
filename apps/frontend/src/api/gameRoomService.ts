88c5cb37fd762f7f2c18c317705b31a498713795
francellaCC <102675185+francellaCC@users.noreply.github.com>
2025-12-01 16:24:44 -0600

    feat: fe- implement enpoitn to get status room

--- apps/frontend/src/api/gameRoomService.ts
+++ apps/frontend/src/api/gameRoomService.ts
@@ -2,15 +2,29 @@ import type { GameRoomRequest, GameRoomResponse } from "../../types";
 import { API } from "./axios";
 
 
+interface roomStatusResponse {
+  ok: boolean;
+  status: string;
+}
 
-export const createGameRoom = async ({languageCode, playerId, code}: GameRoomRequest): Promise<GameRoomResponse> => {
+export const createGameRoom = async ({ languageCode, playerId, code }: GameRoomRequest): Promise<GameRoomResponse> => {
   console.log("API URL:", import.meta.env.VITE_API_URL);
-  console.log("Creating game room with data:", {languageCode, playerId, code});
-  const response = await API.post<GameRoomResponse>("rooms/", {languageCode, playerId, code} );
+  console.log("Creating game room with data:", { languageCode, playerId, code });
+  const response = await API.post<GameRoomResponse>("rooms/", { languageCode, playerId, code });
   return response.data;
 }
 
-export const getGameRoom =async(code: GameRoomRequest['code']) : Promise<GameRoomResponse>=>{
-   const response = await API.get<GameRoomResponse>(`rooms/${code}`);
-   return response.data
+export const getGameRoom = async (code: GameRoomRequest['code']): Promise<GameRoomResponse> => {
+  const response = await API.get<GameRoomResponse>(`rooms/${code}`);
+  return response.data
+}
+
+export const getRoomStatus = async (code: GameRoomRequest['code']): Promise<roomStatusResponse> => {
+  const response = await API.get<roomStatusResponse>(`rooms/status/${code}`);
+  return response.data
+}
+
+export const updateRoomStatus = async (code: GameRoomRequest['code'], status: string): Promise<string> => {
+  const response = await API.post<string>(`rooms/status/${code}`, { status });
+  return response.data
 }
--- apps/frontend/src/components/chat/index.tsx
+++ apps/frontend/src/components/chat/index.tsx
@@ -7,7 +7,6 @@ function ChatComponent({message}: {message?: string}) {
 
  
   return (<div className="chat-component">
-    <h2>Chat Component</h2>
     <div className="flex gap-2 py-2 animate-in fade-in slide-in-from-bottom-2 duration-200">
       <div className="flex-1">
         <div className="flex items-baseline gap-2">
